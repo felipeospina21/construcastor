9c5c34b9e2b470920d1272d4337c10c6edd71b45
felipe ospina calle <felipeospina21@hotmail.com>
2021-02-04 19:08:39 -0500

    add MenuIcons comp

---
+++ src/components/MenuIcons.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Wrap, WrapItem, Center, Container, Icon } from "@chakra-ui/react"
+
+import { GiSteak, GiCupcake } from "react-icons/gi"
+import { BiDrink } from "react-icons/bi"
+import { FaMugHot, FaHamburger } from "react-icons/fa"
+import {BiBox} from 'react-icons/bi'
+
+const MenuIcons = () => {
+const icons = [BiBox,GiSteak,FaHamburger, GiCupcake, BiDrink, FaMugHot, ]
+
+  return (
+    <Center my="4rem" mx={["0", "1rem", "3rem", "auto"]} maxW="950px">
+      <Wrap>
+        {icons.map(icon => {
+          return (
+            <WrapItem m="2rem">
+              <Icon boxSize="5rem" as={icon} />
+            </WrapItem>
+          )
+        })}
+      </Wrap>
+    </Center>
+  )
+}
+
+export default MenuIcons
