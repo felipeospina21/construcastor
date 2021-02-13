491c04a26e1c51d7a4c55f3f4c7342ef8770fb3c
felipe ospina calle <felipeospina21@hotmail.com>
2021-02-12 21:33:44 -0500

    add mirador page

---
+++ src/pages/mirador.js
@@ -0,0 +1,36 @@
+import React from "react"
+import SEO from "../components/seo"
+import Hero from "../components/Hero"
+import { Box, Text, Center } from "@chakra-ui/react"
+import "@ptkdev/webcomponent-instagram-widget"
+
+const MiradorPage = () => {
+  return (
+    <>
+      <SEO title="mirador búho" />
+      <Hero text="Mirador Búho" src="s7.jpg" alt="foto del mirador de búho" />
+      <Box>
+        <Center m="2rem auto" flexDir="column" maxW="1100px">
+          <Center m="0 0.8rem" flexDir="column">
+            <Text fontSize={[null, "2xl"]}>
+              Estamos al lado de Búho, pero somos otro parche.
+            </Text>
+            <Text fontSize={[null, "2xl"]}>
+              Cerveza, amigos, cocteles, buena música y ¡LA MEJOR VISTA DE LA
+              CIUDAD!. Nuestro Mirador Búho, es un espacio increible para
+              disfrutar con tus amigos y compartir momentos agradables, todo en
+              un lugar mágico, inmerso en medio de un bosque.
+            </Text>
+          </Center>
+        </Center>
+      </Box>
+      <instagram-widget
+        username="@miradorbuho"
+        items-limit={6}
+        grid="responsive"
+      ></instagram-widget>
+    </>
+  )
+}
+
+export default MiradorPage
